61c595af21f7c3d168beb459a6aa341393078ea1
Ttruee <kjswksthf76@gmail.com>
2022-11-04 15:14:00 +0900

    주석생김

--- app.py
+++ app.py
@@ -1,40 +1,33 @@
-from flask import Flask, request, make_response  # 서버 구현을 위한 Flask 객체 import
-from flask_restx import Api, Resource, Namespace, fields  # Api 구현을 위한 Api 객체 import
-import json
+from flask import Flask, request
+from flask_restx import Api, Resource
 import pickle
 import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
 
-api = Api(
-    app,
-    version='1.0',
-    title="타이틀",
-    description="설명",
-    terms_url="/",
-    contact="",
-    license="MIT",
-)
+api = Api(app)
 
 
 model = pickle.load(open('lm.pkl','rb'))
 
+# 입력 형태에 맞게 만들어주는 함수
 def _create_input(_quarter, _code, _ser_code, _n):
-    return pd.Series({
+    return [np.array(pd.Series({
         'quarter': _quarter,
         'code': _code,
         'ser_code': _ser_code,
         'n': _n
-    })
+    }))]
 
+# 입력 받은 값으로 예측한 결과값을 출력
 def predict_value(_quarter, _code, _ser_code, _n):
     data_input = _create_input(_quarter, _code, _ser_code, _n)
-    print(data_input)
+    result_value = model.predict(data_input)
 
-    result_value = model.predict([np.array(data_input)])
     return result_value[0]
 
+# 클라이언트 요청 처리함수 > 클라이언트가 지정변수 값을 담아서 서버가 인식할 수 있게 보내줌
 @api.route('/api')
 class getAllData(Resource):
     @api.response(200, 'Success')
@@ -49,6 +42,8 @@ class getAllData(Resource):
         return {
             "result": predict_value(quarter, code, ser_code, n)
         }
-
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=80)
+        
+app.run(debug=True, host='0.0.0.0', port=80)
+# 이 파일을 직접 실행할 경우에 main에 있는 걸 실행 
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=80)
